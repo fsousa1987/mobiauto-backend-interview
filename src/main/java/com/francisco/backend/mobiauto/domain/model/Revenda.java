c72f263dbe2108c852f7e1dd8f0f105ed958e72f
Francisco Sousa <franciscoeds1987@gmail.com>
2024-06-26 08:19:23 -0300

    Entidade Revenda criada

---
+++ src/main/java/com/francisco/backend/mobiauto/domain/model/Revenda.java
@@ -0,0 +1,27 @@
+package com.francisco.backend.mobiauto.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import java.util.UUID;
+
+@Entity
+@Data
+public class Revenda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank(message = "CNPJ é obrigatório")
+    @CNPJ
+    @Column(unique = true, nullable = false)
+    private String cnpj;
+
+    @NotBlank(message = "Nome social é obrigatório")
+    @Column(nullable = false)
+    private String nomeSocial;
+
+}
